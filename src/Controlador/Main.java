d5ad736904b1fa622f87159e1cca108faae4bfb7
InoriHime <jbenito@ciclos.org>
2015-05-16 01:15:38 +0200

    Funciona Login y registro

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -1,10 +1,12 @@
 package Controlador;
 
 import Vista.Login;
+import Vista.VentanaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
         //ejecuta el controlador y este la vista
         new ControladorLogin(new Login()).iniciar();
+        //new ControladorPrincipal(new VentanaPrincipal()).iniciar();
     }
 }
